4a8148e584ababfe34d3f61d1f60b61c7d254349
Raul David <raul.david@amentum.net>
2025-07-05 09:17:48 -0600

    restructuring project
    se eliminan archivos para reestructurar el proyecto

--- contacto/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactoForm(forms.Form):
-    nombre = forms.CharField(max_length=255, label='Nombre')
-    telefono = forms.IntegerField(label='Teléfono celular')
-    email = forms.EmailField(label='Correo electrónico')
--- contacto/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import enviar, index
-    
-urlpatterns = [
-    path('', index, name = 'contacto'),
-    path('enviar', enviar, name='enviar'),
-]
